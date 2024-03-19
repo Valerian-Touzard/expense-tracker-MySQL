5f37a8d5a407a22207caff28b2a964d438738d58
vtouzard <vtouzard244@gmail.com>
2024-03-19 16:14:42 +0100

    fix: Correction de l'import pour la transaction

--- src/main/java/fr/Valerian/ExpenseTracker/repository/TransactionReposi.java
+++ src/main/java/fr/Valerian/ExpenseTracker/repository/TransactionReposi.java
@@ -1,6 +1,6 @@
 package fr.Valerian.ExpenseTracker.repository;
 
-import jakarta.transaction.Transaction;
+import fr.Valerian.ExpenseTracker.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionReposi extends JpaRepository<Transaction, Long> {
