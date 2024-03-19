2492fd972f56e8dbadbafad8c54c54d572dfae26
vtouzard <vtouzard244@gmail.com>
2024-03-20 00:15:55 +0100

    fix: Correction de la route pour accéder au data pour respecter les normes REST

--- src/main/java/fr/Valerian/ExpenseTracker/controller/TransactionController.java
+++ src/main/java/fr/Valerian/ExpenseTracker/controller/TransactionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 public class TransactionController {
     private final TransactionService transactionService;
 
