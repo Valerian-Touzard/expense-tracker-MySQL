ecc606db8060858a12911112aba5cdd9f1255781
vtouzard <vtouzard244@gmail.com>
2024-03-19 16:34:14 +0100

    feat: Mise en place du contoller

---
+++ src/main/java/fr/Valerian/ExpenseTracker/controller/TransactionController.java
@@ -0,0 +1,27 @@
+package fr.Valerian.ExpenseTracker.controller;
+
+import fr.Valerian.ExpenseTracker.model.Transaction;
+import fr.Valerian.ExpenseTracker.service.TransactionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/create")
+    public Transaction add(@RequestBody Transaction transaction) {
+        return transactionService.add(transaction);
+    }
+
+    @GetMapping("")
+    public List<Transaction> getAllTransaction() {
+        return transactionService.getAllTransaction();
+    }
+}
